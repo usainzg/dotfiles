ab692696d121a7d57d7bd2e1db1e66160b3522b2
jjasmine <tanghocle456@gmail.com>
2024-07-02 12:42:11 -0500

    .

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,13 +1,15 @@
 return {
     "nvim-lualine/lualine.nvim",
 
-	config = function()
-		require('lualine').setup{
-                  options = {
-                    theme = "catppuccin"
-                  }
+    config = function()
+        require('lualine').setup {
+            sections = {
+                lualine_x = { require("action-hints").statusline },
+            },
+            options = {
+                theme = "catppuccin"
             }
-
+        }
     end
 
 }
