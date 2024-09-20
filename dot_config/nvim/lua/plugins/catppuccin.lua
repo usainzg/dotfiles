79ba5011d71915df842199618b0b9a4f5a6d5f85
badumbatish <tanghocle456@gmail.com>
2024-09-20 12:20:23 -0700

    ok

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -1,14 +1,12 @@
 return {
-    {
-        "catppuccin",
-        opts = {
-            transparent_background = true,
-        },
-    },
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "catppuccin",
-        },
-    },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            transparent_background = true, -- disables setting the background color.
+
+        })
+        vim.cmd.colorscheme "catppuccin-mocha"
+    end
 }
