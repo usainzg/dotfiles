ad0220a73f5521bd96538f82714b3141196f696f
badumbatish <tanghocle456@gmail.com>
2024-10-17 02:01:51 -0700

    ok add things?

--- dot_config/nvim/lua/configs/options.lua
+++ dot_config/nvim/lua/configs/options.lua
@@ -12,7 +12,7 @@ vim.opt.scrolloff      = 10
 vim.opt.tabstop        = 8
 vim.opt.softtabstop    = 4
 vim.opt.shiftwidth     = 4
-vim.opt.expandtab      = true
+-- vim.opt.expandtab      = true
 vim.opt.incsearch      = true
 
 vim.opt.numberwidth    = 4
