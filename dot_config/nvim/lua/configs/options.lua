5fe7bdac9930999533bf580e9dc21da8b482d167
badumbatish <tanghocle456@gmail.com>
2025-06-02 02:33:06 -0700

    ok

--- dot_config/nvim/lua/configs/options.lua
+++ dot_config/nvim/lua/configs/options.lua
@@ -52,3 +52,14 @@ vim.api.nvim_create_autocmd("FileChangedShellPost", {
         } }, true, {})
     end
 })
+
+
+--- Open file at the last position it was edited earlier
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+            vim.api.nvim_win_set_cursor(0, mark)
+        end
+    end,
+})
