aa8f32216ea0b8d29da8446a0c597d13a3fa5666
badumbatish <tanghocle456@gmail.com>
2025-06-03 18:05:50 -0700

    add an installer

---
+++ install.sh
@@ -0,0 +1,8 @@
+
+sudo pacman -Syu zsh zsh-autosuggestions zsh-syntax-highlighting neovim rust cargo atuin python3 nodejs npm fzf tmux unzip make cmake
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+curl -sS https://starship.rs/install.sh | sh
+
+
