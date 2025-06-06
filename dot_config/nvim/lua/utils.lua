4e902f58c49b5e7f7aa785c637203be7e80c45b7
badumbatish <tanghocle456@gmail.com>
2025-06-06 11:28:57 -0700

    ok

--- dot_config/nvim/lua/utils.lua
+++ dot_config/nvim/lua/utils.lua
@@ -1,31 +1,31 @@
 local M = {}
 
 M.extract_vis_text = function()
-	local current_mode = vim.fn.mode()
+    local current_mode = vim.fn.mode()
 
-	local start_info = vim.fn.getpos("v")
-	local end_info = vim.fn.getpos(".")
+    local start_info = vim.fn.getpos("v")
+    local end_info = vim.fn.getpos(".")
 
-	vim.print(current_mode)
-	-- if its the whole line, always get from beginning
-	if current_mode == "V" then
-		if (start_info[2] == end_info[2]) then
-			return vim.fn.getline(start_info[2])
-		else
-			return table.concat(vim.fn.getline(start_info[2], end_info[2]), "\n")
-		end
-	elseif current_mode == "v" then
-		return table.concat(vim.fn.getregion(start_info, end_info), "\n")
-	else
-		vim.print("Failure to select in visual mode")
-	end
+    vim.print(current_mode)
+    -- if its the whole line, always get from beginning
+    if current_mode == "V" then
+        if (start_info[2] == end_info[2]) then
+            return vim.fn.getline(start_info[2])
+        else
+            return table.concat(vim.fn.getline(start_info[2], end_info[2]), "\n")
+        end
+    elseif current_mode == "v" then
+        return table.concat(vim.fn.getregion(start_info, end_info), "\n")
+    else
+        vim.print("Failure to select in visual mode")
+    end
 end
 
 
 M.jasmine_escape = function(str)
-	--find .  -type f | xargs sed -i 's/\#include "ast\/ast.h"/haha/g'
-	local escaped = vim.fn.escape(str, "/#")
-	return escaped
+    --find .  -type f | xargs sed -i 's/\#include "ast\/ast.h"/haha/g'
+    local escaped = vim.fn.escape(str, "/#")
+    return escaped
 end
 
 return M
