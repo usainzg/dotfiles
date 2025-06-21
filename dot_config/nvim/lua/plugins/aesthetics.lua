80bb83a4702d8e43a082107d6779b6206159a3ee
badumbatish <jjasmine@igalia.com>
2025-06-21 00:43:03 -0700

    Automatic update via ~/.local/bin/cadd

---
+++ dot_config/nvim/lua/plugins/aesthetics.lua
@@ -0,0 +1,77 @@
+return {
+    {
+        -- SHOWS TAB LINE
+        'crispgm/nvim-tabline',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }, -- optional
+        config = true,
+    },
+
+    {
+        -- SHOWS STATUS LINE AT THE BOTTOM
+        "nvim-lualine/lualine.nvim",
+
+        config = function()
+            require('lualine').setup {
+                options = {
+                    theme = 'ayu',
+                }
+            }
+        end
+
+
+    },
+
+    {
+        -- SHOWS THE FILE'S HIARARCHY TREE
+        "utilyre/barbecue.nvim",
+        name = "barbecue",
+        version = "*",
+        dependencies = {
+            "SmiteshP/nvim-navic",
+            "nvim-tree/nvim-web-devicons", -- optional dependency
+        },
+        opts = {
+            -- configurations go here
+        },
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            -- require("catppuccin").setup({
+            --     transparent_background = true, -- disables setting the background color.
+            --
+            -- })
+            -- --- latte for light
+            -- --- mocha for dark
+            -- vim.cmd.colorscheme "catppuccin-mocha"
+        end
+    },
+    {
+        "Shatur/neovim-ayu",
+        priority = 1000,
+        config = function()
+            require('ayu').setup({
+                mirage = false,  -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+                terminal = true, -- Set to `false` to let terminal manage its own colors.
+                overrides = {},  -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+            })
+            require('ayu').colorscheme()
+        end
+    },
+
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
+
+    {
+        "HiPhish/rainbow-delimiters.nvim",
+    }
+}
---
+++ dot_config/nvim/lua/plugins/gits.lua
@@ -0,0 +1,94 @@
+return {
+    {
+        "tpope/vim-fugitive"
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require('gitsigns').setup {
+                signs                        = {
+                    add          = { text = '┃┃' },
+                    change       = { text = '┃┃' },
+                    delete       = { text = '__' },
+                    topdelete    = { text = '‾‾' },
+                    changedelete = { text = '~~' },
+                    untracked    = { text = '┆┆' },
+                },
+                signs_staged                 = {
+                    add          = { text = '┃┃' },
+                    change       = { text = '┃┃' },
+                    delete       = { text = '__' },
+                    topdelete    = { text = '‾‾' },
+                    changedelete = { text = '~~' },
+                    untracked    = { text = '┆┆' },
+                },
+                signs_staged_enable          = true,
+                signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
+                numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
+                linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
+                word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+                watch_gitdir                 = {
+                    follow_files = true
+                },
+                auto_attach                  = true,
+                attach_to_untracked          = false,
+                current_line_blame           = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+                current_line_blame_opts      = {
+                    virt_text = true,
+                    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+                    delay = 1000,
+                    ignore_whitespace = false,
+                    virt_text_priority = 100,
+                },
+                current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+                sign_priority                = 6,
+                update_debounce              = 100,
+                status_formatter             = nil,   -- Use default
+                max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+                preview_config               = {
+                    -- Options passed to nvim_open_win
+                    border = 'single',
+                    style = 'minimal',
+                    relative = 'cursor',
+                    row = 0,
+                    col = 1
+                },
+                on_attach                    = function(bufnr)
+                    local gitsigns = require('gitsigns')
+
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+                    map('n', 'gb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
+                    -- map('n', '<leader>d', function() gitsigns.diffthis('~') end)
+                end
+            }
+        end,
+    },
+
+    { -- GIT BLAME
+        "f-person/git-blame.nvim",
+        -- load the plugin at startup
+        event = "VeryLazy",
+        -- Because of the keys part, you will be lazy loading this plugin.
+        -- The plugin wil only load once one of the keys is used.
+        -- If you want to load the plugin at startup, add something like event = "VeryLazy",
+        -- or lazy = false. One of both options will work.
+        opts = {
+            -- your configuration comes here
+            -- for example
+            enabled = true, -- if you want to enable the plugin
+            message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+            date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+            virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+        },
+
+        vim.keymap.set('n', '<leader>yh', "<cmd> GitBlameCopySHA<CR>")
+
+
+
+
+    }
+}
