a0587d6b23dad95e3ce04ba1bbb675fb10f6fa88
badumbatish <jjasmine@igalia.com>
2025-06-21 01:10:54 -0700

    Automatic update via ~/.local/bin/cadd

--- dot_config/nvim/lua/plugins/yazi.lua
+++ dot_config/nvim/lua/plugins/yazi.lua
@@ -9,5 +9,16 @@ return
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+
     lazy = false,
+
+    config = function()
+        require("oil").setup({
+            keymaps = {
+                ["-"] = { "actions.parent", mode = "n" },
+                ["h"] = { "actions.parent", mode = "n" },
+                ["l"] = "actions.select",
+            },
+        })
+    end
 }
