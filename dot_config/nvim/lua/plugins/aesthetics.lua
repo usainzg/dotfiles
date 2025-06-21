878f899bc0a82bd6982fc81610ef9b602f585d03
badumbatish <jjasmine@igalia.com>
2025-06-21 02:30:50 -0700

    Automatic update via ~/.local/bin/cadd

--- dot_config/nvim/lua/plugins/aesthetics.lua
+++ dot_config/nvim/lua/plugins/aesthetics.lua
@@ -27,7 +27,28 @@ return {
 
 
     },
+    {
+        "m00qek/baleia.nvim",
+        version = "*",
+        config = function()
+            vim.g.baleia = require("baleia").setup({})
 
+            -- Command to colorize the current buffer
+            vim.api.nvim_create_user_command("BaleiaColorize", function()
+                vim.g.baleia.once(vim.api.nvim_get_current_buf())
+            end, { bang = true })
+
+            -- Command to show logs
+            vim.api.nvim_create_user_command("BaleiaLogs", vim.g.baleia.logger.show, { bang = true })
+
+            vim.api.nvim_create_autocmd({ "BufEnter" }, {
+                pattern = "*.txt",
+                callback = function()
+                    vim.cmd("BaleiaColorize")
+                end,
+            })
+        end,
+    },
     {
         -- SHOWS THE FILE'S HIARARCHY TREE
         "utilyre/barbecue.nvim",
