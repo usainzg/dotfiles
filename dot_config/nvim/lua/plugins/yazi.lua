946a1ca7fa86a00d4f0c4857b36e4b28b7facdf7
badumbatish <jjasmine@igalia.com>
2025-06-21 01:23:06 -0700

    Automatic update via ~/.local/bin/cadd

--- dot_config/nvim/lua/plugins/yazi.lua
+++ dot_config/nvim/lua/plugins/yazi.lua
@@ -11,8 +11,17 @@ return
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 
     lazy = false,
-
     config = function()
+        local oil = require("oil")
+        local actions = require("oil.actions")
+        local function smart_select()
+            local entry = oil.get_cursor_entry()
+            if entry and entry.type == "directory" then
+                actions.select.callback()
+            else
+                actions.select.callback({ tab = true })
+            end
+        end
         require("oil").setup({
             columns = {
                 "icon",
@@ -20,11 +29,18 @@ return
                 -- "size",
                 -- "mtime",
             },
+
+            -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
+            -- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
+            -- Additionally, if it is a string that matches "actions.<name>",
+            -- it will use the mapping at require("oil.actions").<name>
+            -- Set to `false` to remove a keymap
+            -- See :help oil-actions for a list of all available actions
             keymaps = {
                 ["-"] = { "actions.parent", mode = "n" },
                 ["h"] = { "actions.parent", mode = "n" },
-                ["<CR>"] = { "actions.select", opts = { tab = true } },
-                ["l"] = "actions.select",
+                ["<CR>"] = { callback = smart_select },
+                ["l"] = { callback = smart_select },
             },
         })
     end
