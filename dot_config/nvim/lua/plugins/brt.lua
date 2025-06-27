9dd0ad2be24dc7ae6b2b663248fbb1303bcb6cbd
badumbatish <jjasmine@igalia.com>
2025-06-27 12:31:36 -0700

    Automatic update via ~/.local/bin/cadd

--- dot_config/nvim/lua/plugins/brt.lua
+++ dot_config/nvim/lua/plugins/brt.lua
@@ -1,8 +1,8 @@
 return {
 	"badumbatish/brt.nvim",
 	-- -- Uncomment these two lines to contribute and develop
-	-- dir = "~/Developer/nvim_proj/brt.nvim",
-	-- dev = { true },
+	dir = "~/Developer/nvim_proj/brt.nvim",
+	dev = { true },
 	-- @t
 	config = function()
 		local brt = require("brt")
